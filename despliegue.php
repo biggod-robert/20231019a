eef07810689663a4afe9827e08856f4924955e85
biggod-robert <128723161+biggod-robert@users.noreply.github.com>
2023-10-19 08:13:40 -0500

    avance 4.1

---
+++ despliegue.php
@@ -0,0 +1,14 @@
+<?php
+
+//aqui se incluye el archivo de funciones
+include('funciones.php');
+
+//invoca la funcion consulta
+echo consultas();
+echo "<br>";
+//se invoca la funcion viajar
+echo viajar();
+echo "<br>";//hace un salto de linea
+
+echo unnuevoregistro();//se invoca la funcion unnuevoregistro
+?>
---
+++ funciones.php
@@ -0,0 +1,47 @@
+<?php
+
+//se crea funcion para una consulta
+function consultas(){
+
+    $salida= "";//incializa la variable
+    $salida= 10*2/2; //calcula la operacion del area de un triangulo
+    
+
+    return $salida;//retorna la operacion
+    
+}
+//esta es una nueva funcion
+function viajar(){
+
+    $exit= 0;// se inicializa la variable
+
+    $exit= 4**4+10**2;//se realiza el calculo del perimetro de un rectangulo
+
+    return $exit;//retorna el calculo
+}
+
+function unnuevoregistro(){
+
+    $exit = 0;// se inicializa la variable
+    //se hace la conexion con la base de datos
+    $conexion= new mysqli('localhost', 'root', 'root', 'tour_people');
+
+    $sql = "select 2+1 ";
+    $sql .= "as suma";//
+    $resultado = $conexion->query($sql);
+
+    //recorre el recordset
+    while($fila = mysqli_fetch_assoc($resultado))
+    {
+        //incluye o acumula
+        $exit += $fila['suma'];
+    }
+    return $exit;//retorna
+     
+    $conexion->close()
+
+    
+}
+
+
+?>

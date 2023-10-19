215baf72de95b59ae7ce5d4f24f71f21732d9823
biggod-robert <128723161+biggod-robert@users.noreply.github.com>
2023-10-19 07:20:16 -0500

    avance 2

---
+++ 20231019a/20231019a/despliegue.php
@@ -0,0 +1,9 @@
+<?php
+
+//aqui se incluye el archivo de funciones
+include('funciones.php');
+
+//invoca la funcion consulta
+echo consultas();
+
+?>
---
+++ 20231019a/20231019a/funciones.php
@@ -0,0 +1,14 @@
+<?php
+
+//se crea funcion para una consulta
+function consultas(){
+
+    $salida= "";//incializa la variable
+    $salida= 10*2/2; //calcula la operacion del area de un triangulo
+    
+
+    return $salida;//retorna la operacion
+    
+}
+
+?>

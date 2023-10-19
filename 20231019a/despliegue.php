87d9e918617798b7264954b4457641cf4e16f87f
biggod-robert <128723161+biggod-robert@users.noreply.github.com>
2023-10-19 07:29:48 -0500

    avance 3

--- 20231019a/despliegue.php
+++ 20231019a/despliegue.php
@@ -1,7 +1,10 @@
 <?php
 
 //aqui se incluye el archivo de funciones
-include('funciones.php')
+include('funciones.php');
 
-
+//invoca la funcion consulta
+echo consultas();
+//se invoca la funcion viajar
+echo viajar();
 ?>
--- 20231019a/funciones.php
+++ 20231019a/funciones.php
@@ -1,6 +1,23 @@
 <?php
 
-//se imprime un mensaje para saber si funciona
-echo "hola";
+//se crea funcion para una consulta
+function consultas(){
+
+    $salida= "";//incializa la variable
+    $salida= 10*2/2; //calcula la operacion del area de un triangulo
+    
+
+    return $salida;//retorna la operacion
+    
+}
+//esta es una nueva funcion
+function viajar(){
+
+    $exit= 0;// se inicializa la variable
+
+    $exit= 4**4+10**2;//se realiza el calculo del perimetro de un rectangulo
+
+    return $exit;//retorna el calculo
+}
 
 ?>
